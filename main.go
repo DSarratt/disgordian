be2944c29b67ef85b72e87b2286cb6301fbd33cf
Donald Sarratt <dsarratt@gmail.com>
2016-10-14 23:43:08 +1300

    Open a websocket, receive the HELLO and extract heartbeat interval.

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"golang.org/x/net/websocket"			// Go get golang.org/x/net/websocket
+	"golang.org/x/net/websocket" // Go get golang.org/x/net/websocket
 	"io"
 	"io/ioutil"
 	"log"
@@ -26,13 +26,14 @@ var (
 	Error   *log.Logger
 )
 
+// LogInit sets up the three logging functions (Debug, Info, Error).
+// Thanks to William Kennedy - https://www.goinggo.net/2013/11/using-log-package-in-go.html
 func LogInit(
-	// Thanks to William Kennedy
-	// https://www.goinggo.net/2013/11/using-log-package-in-go.html
 	debugHandle io.Writer,
 	infoHandle io.Writer,
 	errorHandle io.Writer) {
 
+	// TODO: Disable if this impacts performance
 	Debug = log.New(debugHandle,
 		"DEBUG: ",
 		log.Ldate|log.Ltime|log.Lshortfile)
@@ -47,13 +48,10 @@ func LogInit(
 }
 
 func main() {
-	// TODO: Read config from file
-	config := make(map[string]interface{})
-	config["bot_token"] = ""
-	
+	// TODO: Scoping/break into functions
 	// Setup logging
 	LogInit(os.Stdout, os.Stdout, os.Stdout)
-	
+
 	// GET request to the Discordian API, to find the gateway URL
 	resp, err := http.Get(BASE_URL + "/gateway")
 	if err != nil {
@@ -62,32 +60,68 @@ func main() {
 	if resp.StatusCode != 200 {
 		panic(fmt.Sprintf("Received non-200 status code %d from gateway URL fetch", resp.StatusCode))
 	}
+
 	// Read response into a buffer
-	// If we get more than 2kB of response something has gone wrong
 	body, err := ioutil.ReadAll(resp.Body)
 	Debug.Printf("Received %d bytes from gateway URL fetch\n", len(body))
+
 	// Decode the JSON
-	var output_map map[string]interface{}
+	var output_map map[string]string
 	err = json.Unmarshal(body, &output_map)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Couldn't decode HTTP response: %q", err))
 	}
 	url, ok := output_map["url"]
 	if !ok {
 		panic("Didn't receive a url from the gateway URL fetch")
 	}
 	Debug.Printf("Received gateway URL: %q", url)
-	
-	// Initialise plugins
+
+	// TODO: Initialise plugins
 	//var pluginlist []func(<-chan *map[string]interface{})
 	// Dynamic plugin loading: functions can be first-class, but not packages.
 	// Define a startup script that writes a .go file loading all the plugin.Start()
 	// functions into an array, then recompiles the bot. Ugh?
 	// The 'plugins' package would be nice, but it's only in dev...
-	
+
 	// Start websocket
-	
-	// Login, hello
-	
-	// Enter read loop
+	ws, err := websocket.Dial(url+GATEWAY_VERSION, "", "https://discordapp.com")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open websocket: %q", err))
+	}
+	defer ws.Close()
+	Debug.Printf("Websocket opened")
+
+	// TODO: get websocket.JSON working?
+	/*
+		var data Payload
+		websocket.JSON.Receive(ws, &data)
+		fmt.Println(data)
+	*/
+	// Receive first packet, decode from JSON
+	// TODO: Timeout on socket read
+	var buffer []byte
+	websocket.Message.Receive(ws, &buffer)
+	Debug.Printf("Received websocket payload: %q", buffer)
+	var payload map[string]interface{}
+	err = json.Unmarshal(buffer, &payload)
+	if err != nil {
+		panic(fmt.Sprintf("Couldn't decode websocket payload: %q", err))
+	}
+	dload, ok := payload["d"].(map[string]interface{})
+	if !ok {
+		panic(fmt.Sprintf("Couldn't get event data from payload: %q", payload["d"]))
+	}
+	temp, ok := dload["heartbeat_interval"].(float64)
+	if !ok {
+		panic(fmt.Sprintf("Couldn't get heartbeat from payload: %q", dload))
+	}
+	heartbeat := int(temp)
+	Debug.Printf("Heartbeat: %dms", heartbeat)
+
+	// TODO: Login
+
+	// TODO: Go run heartbeats
+
+	// TODO: Enter read loop
 }
